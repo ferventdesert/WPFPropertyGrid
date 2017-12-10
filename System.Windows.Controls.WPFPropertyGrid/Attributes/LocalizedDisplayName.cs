2035fbb269fbbc672f9749d47852e667b5924b84
Rain <grs4321@qq.com>
2017-12-10 12:24:30 +0800

    add lost file

---
+++ System.Windows.Controls.WPFPropertyGrid/Attributes/LocalizedDisplayName.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace System.Windows.Controls.WpfPropertyGrid.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Event)]
+    public class LocalizedDisplayNameAttribute : DisplayNameAttribute
+    {
+        public LocalizedDisplayNameAttribute(string key) : base(key)
+        {
+        }
+    }
+    [AttributeUsage(AttributeTargets.All)]
+    public class LocalizedDescriptionAttribute : DescriptionAttribute
+    {
+        public LocalizedDescriptionAttribute(string key) : base(key)
+        {
+            
+        }
+    }
+    [AttributeUsage(AttributeTargets.All)]
+    public class LocalizedCategoryAttribute : CategoryAttribute
+    {
+        public LocalizedCategoryAttribute(string key) : base(key)
+        {
+        }
+
+        protected override string GetLocalizedString(string value)
+        {
+            return value;
+        }
+    }
+}
